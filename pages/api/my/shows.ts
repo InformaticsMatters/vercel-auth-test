f5757803b11943f6b1be766cc6e62bf4ab0cac99
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2023-07-08 14:27:56 +0100

    fix ts error

--- pages/api/my/shows.ts
+++ pages/api/my/shows.ts
@@ -1,19 +1,23 @@
-import { NextJwtVerifier } from '@serverless-jwt/next';
-import { NextAuthenticatedApiRequest } from '@serverless-jwt/next/dist/types';
-import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
+import { NextJwtVerifier } from "@serverless-jwt/next";
+import { NextAuthenticatedApiRequest } from "@serverless-jwt/next/dist/types";
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 
 const verifyJwt = NextJwtVerifier({
-  issuer: process.env.AUTH0_ISSUER_BASE_URL,
-  audience: process.env.AUTH0_AUDIENCE
+  issuer: process.env.AUTH0_ISSUER_BASE_URL!,
+  audience: process.env.AUTH0_AUDIENCE!,
 });
 
 const requireScope = (scope: string, apiRoute: NextApiHandler) =>
   verifyJwt(async (req: NextAuthenticatedApiRequest, res) => {
     const { claims } = req.identityContext;
-    if (!claims || !claims.scope || (claims.scope as string).indexOf(scope) === -1) {
+    if (
+      !claims ||
+      !claims.scope ||
+      (claims.scope as string).indexOf(scope) === -1
+    ) {
       return res.status(403).json({
-        error: 'access_denied',
-        error_description: `Token does not contain the required '${scope}' scope`
+        error: "access_denied",
+        error_description: `Token does not contain the required '${scope}' scope`,
       });
     }
     return apiRoute(req, res) as void;
@@ -21,7 +25,9 @@ const requireScope = (scope: string, apiRoute: NextApiHandler) =>
 
 const apiRoute = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const response = await fetch('https://api.tvmaze.com/search/shows?q=identity');
+    const response = await fetch(
+      "https://api.tvmaze.com/search/shows?q=identity"
+    );
     const shows = await response.json();
 
     res.status(200).json({ shows });
@@ -29,9 +35,9 @@ const apiRoute = async (req: NextApiRequest, res: NextApiResponse) => {
     console.error(error);
     res.status(error.status || 500).json({
       code: error.code,
-      error: error.message
+      error: error.message,
     });
   }
 };
 
-export default requireScope('read:shows', apiRoute);
+export default requireScope("read:shows", apiRoute);
